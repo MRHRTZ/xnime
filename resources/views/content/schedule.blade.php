abe02ee13afecb6ed5149cc886d2f04b672387ee
MRHRTZ <hanifsyauqi61@gmail.com>
2023-12-01 07:47:13 +0700

    Fix Schedule Card 📇

--- resources/views/content/schedule.blade.php
+++ resources/views/content/schedule.blade.php
@@ -9,55 +9,57 @@
     <div class="swiper mySwiper">
         <div class="swiper-wrapper">
             @foreach ($days as $day)
-            <div id="day-{{ $day['no'] }}" class="swiper-slide schedule-panel card">
-                <span class="day">{{ $day['day'] }}</span>
-                <span class="week"></span>
-                <div class="anime-list">
-                    @foreach ($schedules->{$day['no']} as $schedule)
-                    <div class="anime-item">
-                        @auth
-                        @php
-                        $history = findObjectByCustomId($history_list, $schedule->id, 'anime_id');
-                        @endphp
-                        @endauth
-                        @if (isset($history))
-                        @if ($history)
-                        <a target="_self"
-                            href="{{ route('episodes', ['anime_id'=>$history->anime_id,'episode_id'=>$history->episode_id,'server_id'=>$history->server_id]) }}">
-                            <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
-                                src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
-                            <br>
-                            <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
-                                class="title">{!!
-                                htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
-                                !!}</a>
-                        </a>
-                        @else
-                        <a target="_self"
-                            href="{{ route('episodes', ['anime_id'=>$schedule->id,'episode_id'=>'0']) }}">
-                            <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
-                                src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
-                            <br>
-                            <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
-                                class="title">{!!
-                                htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
-                                !!}</a>
-                        </a>
-                        @endif
-                        @else
-                        <a target="_self"
-                            href="{{ route('episodes', ['anime_id'=>$schedule->id,'episode_id'=>'0']) }}">
-                            <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
-                                src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
-                            <br>
-                            <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
-                                class="title">{!!
-                                htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
-                                !!}</a>
-                        </a>
-                        @endif
+            <div id="day-{{ $day['no'] }}" class="swiper-slide">
+                <div class="schedule-panel card">
+                    <span class="day">{{ $day['day'] }}</span>
+                    <span class="week"></span>
+                    <div class="anime-list">
+                        @foreach ($schedules->{$day['no']} as $schedule)
+                        <div class="anime-item">
+                            @auth
+                            @php
+                            $history = findObjectByCustomId($history_list, $schedule->id, 'anime_id');
+                            @endphp
+                            @endauth
+                            @if (isset($history))
+                            @if ($history)
+                            <a target="_self"
+                                href="{{ route('episodes', ['anime_id'=>$history->anime_id,'episode_id'=>$history->episode_id,'server_id'=>$history->server_id]) }}">
+                                <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
+                                    src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
+                                <br>
+                                <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
+                                    class="title">{!!
+                                    htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
+                                    !!}</a>
+                            </a>
+                            @else
+                            <a target="_self"
+                                href="{{ route('episodes', ['anime_id'=>$schedule->id,'episode_id'=>'0']) }}">
+                                <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
+                                    src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
+                                <br>
+                                <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
+                                    class="title">{!!
+                                    htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
+                                    !!}</a>
+                            </a>
+                            @endif
+                            @else
+                            <a target="_self"
+                                href="{{ route('episodes', ['anime_id'=>$schedule->id,'episode_id'=>'0']) }}">
+                                <img onerror="this.src = '{{ url('assets/img/logo/2.png') }}'"
+                                    src="{{ $schedule->image_cover }}" class="swiper-slide__cover">
+                                <br>
+                                <a target="_self" href="{{ route('detail-anime', ['id'=>$schedule->id]) }}"
+                                    class="title">{!!
+                                    htmlspecialchars_decode(htmlspecialchars_decode(html_entity_decode($schedule->title)))
+                                    !!}</a>
+                            </a>
+                            @endif
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
             </div>
             @endforeach
@@ -103,15 +105,15 @@
         breakpoints: {
             640: {
                 slidesPerView: 2,
-                spaceBetween: 20,
+                spaceBetween: 30,
             },
             768: {
                 slidesPerView: 3,
-                spaceBetween: 40,
+                spaceBetween: 30,
             },
             1024: {
                 slidesPerView: 4,
-                spaceBetween: 40,
+                spaceBetween: 30,
             }
         }
     });
